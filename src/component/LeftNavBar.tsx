6bf8a5d0d7abdb5220515ed433029df967eef380
Muage <past_exp@naver.com>
2024-12-20 10:42:02 +0900

    처음으로 기능 추가 SQL 적용 전 Rollback용 Commit

--- src/component/LeftNavBar.tsx
+++ src/component/LeftNavBar.tsx
@@ -1,7 +1,8 @@
 import { Link } from 'react-router-dom'
 import { BiMenu, BiX } from "react-icons/bi";
-import '../assets/css/layout.css'
 import { useEffect } from 'react';
+import { BiArrowFromLeft } from "react-icons/bi";
+import '../assets/css/layout.css'
 
 interface Props {
     isNavOpen: boolean,
@@ -12,6 +13,11 @@ interface Props {
 const LeftNavBar = ({ isNavOpen, setIsNavOpen, toggleNav }: Props) => {
 
     const className = sessionStorage.getItem('class-name')
+
+    const handleIsNavOpen = () => {
+        setIsNavOpen(false)
+        sessionStorage.removeItem('class-name')
+    }
 
     useEffect(() => {
         if(className) {
@@ -24,13 +30,21 @@ const LeftNavBar = ({ isNavOpen, setIsNavOpen, toggleNav }: Props) => {
         {!isNavOpen ? (
             <BiMenu className="btn-menu" size={30} onClick={toggleNav} />
         ) : (
-            <div className={`left-nav-bar ${isNavOpen ? 'open' : 'colsed'}`}>
+            <div className={`left-nav-bar ${isNavOpen && 'open'}`}>
                 {isNavOpen && 
                     <BiX className="btn-close" size={30} onClick={toggleNav} />
                 }
                 <ul>
                     <li><Link to="/entries">뽑기 인원 관리</Link></li>
                     <li><Link to="/raffle">뽑기판</Link></li>
+                </ul>
+                <ul className="list-bottom">
+                    <li onClick={handleIsNavOpen}>
+                        <Link to="/">
+                            <span>처음으로</span>
+                            <BiArrowFromLeft />
+                        </Link>
+                    </li>
                 </ul>
             </div>
         )}
